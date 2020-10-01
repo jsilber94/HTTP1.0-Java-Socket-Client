6f2b286952b84e1c91ac42b84d50d6876fa43a7d
Omnia <ogomaa.concordia@gmail.com>
2020-10-01 15:35:54 -0400

    added parameters for both httpc get/post commands

--- src/concordia/comp445/HttpcGet.java
+++ src/concordia/comp445/HttpcGet.java
@@ -1,4 +1,37 @@
 package concordia.comp445;
+import com.beust.jcommander.*;
 
+import java.util.Map;
+
+@Parameters(commandDescription = "GET request")
 public class HttpcGet {
+
+    @Parameter(names = {"-v"})
+    private Boolean verbose = false;
+
+    @Parameter(names = {"-o"})
+    private String outputFile;
+
+    @Parameter(names = {"-h"})
+    private String header;
+
+    @Parameter(description = "Host name")
+    private String url;
+
+    public Boolean getVerbose() {
+        return verbose;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getOutputFile() {
+        return outputFile;
+    }
 }
+
--- src/concordia/comp445/HttpcPost.java
+++ src/concordia/comp445/HttpcPost.java
@@ -1,8 +1,50 @@
 package concordia.comp445;
 
+import com.beust.jcommander.Parameter;
 
+@Parameters(commandDescription = "POST request")
 public class HttpcPost {
 
-    
+    @Parameter(names = {"-v"})
+    private Boolean verbose = false;
+
+    @Parameter(names = {"-o"})
+    private String outputFile;
+
+    @Parameter(names = {"-h"})
+    private String header;
+
+    @Parameter(names = {"-d"}, forbids={"-f"})
+    private String option;
+
+    @Parameter(names = {"-f"},forbids={"-d"})
+    private String inputFile;
+
+    @Parameter(description = "Host name")
+    private String url;
+
+    public Boolean getVerbose() {
+        return verbose;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getOutputFile() {
+        return outputFile;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public String getInputFile() {
+        return inputFile;
+    }
 
 }
